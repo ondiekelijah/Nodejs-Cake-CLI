f6a9ff67d7c0bed876005a4a2e5fc0ed9a14d5ee
Dev-Elie <eochieng9448@gmail.com>
2022-03-04 10:05:06 +0300

    Updated tests

--- __tests__/order.spec.js
+++ __tests__/order.spec.js
@@ -1,6 +1,5 @@
 const order = require("../lib/order");
-const inquirer = require('inquirer')
-
+const inquirer = require("inquirer");
 
 const orderObj = {
   type: "Vanilla",
@@ -17,55 +16,45 @@ const result = [
 test("Maps properties correctly - commander", () => {
   expect(order.commanderOrder(orderObj)).toEqual(result);
   expect(order.commanderOrder(orderObj).length).toEqual(3);
-  expect(order.commanderOrder(orderObj)[0]).toContain('Vanilla');
-  expect(order.commanderOrder(orderObj)[1]).toContain('Large');
-  expect(order.commanderOrder(orderObj)[2]).toContain('Nuts');
+  expect(order.commanderOrder(orderObj)[0]).toContain("Vanilla");
+  expect(order.commanderOrder(orderObj)[1]).toContain("Large");
+  expect(order.commanderOrder(orderObj)[2]).toContain("Nuts");
 });
 
-
-describe('Order different types of cakes', () => {
-
-  test('order cake: type A', async () => {
-
+describe("Order different types of cakes", () => {
+  test("order cake: type A", async () => {
     cakeA = {
-      'Coating': 'Butter',
-      'type': 'Strawberry',
-      'Cake Size': 'Medium',
-      'Toppings': 'Fruit'
-    }
-    
-    const order_cli = await inquirer.prompt(order.orderDetailsQuestions,cakeA);
+      Coating: "Butter",
+      type: "Strawberry",
+      "Cake Size": "Medium",
+      Toppings: "Fruit",
+    };
+
+    const order_cli = await inquirer.prompt(order.orderDetailsQuestions, cakeA);
     expect(order_cli).toMatchObject(cakeA);
-    
-  })
+  });
 
-
-  test('order cake: type B', async () => {
-
+  test("order cake: type B", async () => {
     cakeB = {
-      'Coating': 'Icing Sugar',
-      'type': 'Vanilla',
-      'Cake Size': 'Large',
-      'Toppings': 'Chocolate'
-    }
-    
-    const order_cli = await inquirer.prompt(order.orderDetailsQuestions,cakeB);
+      Coating: "Icing Sugar",
+      type: "Vanilla",
+      "Cake Size": "Large",
+      Toppings: "Chocolate",
+    };
+
+    const order_cli = await inquirer.prompt(order.orderDetailsQuestions, cakeB);
     expect(order_cli).toMatchObject(cakeB);
-    
-  })
+  });
 
-  test('order cake: type C', async () => {
-
+  test("order cake: type C", async () => {
     cakeC = {
-      'Coating': 'Butter',
-      'type': 'Mint',
-      'Cake Size': 'Small',
-      'Toppings': 'Biscuits'
-    }
-    
-    const order_cli = await inquirer.prompt(order.orderDetailsQuestions,cakeC);
-    expect(order_cli).toMatchObject(cakeC);
-    
-  })
+      Coating: "Butter",
+      type: "Mint",
+      "Cake Size": "Small",
+      Toppings: "Biscuits",
+    };
 
+    const order_cli = await inquirer.prompt(order.orderDetailsQuestions, cakeC);
+    expect(order_cli).toMatchObject(cakeC);
+  });
 });
